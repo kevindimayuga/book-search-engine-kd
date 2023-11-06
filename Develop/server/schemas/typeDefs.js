27231025b5be2d2c507753e4fd3dd583010afe00
kevindimayuga <k.dimayuga22@gmail.com>
2023-11-05 22:23:43 -0600

    updated typeDefs

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,7 +1,49 @@
+// Define the necessary typeDefs for the Query and Mutation
+// functionality of the Book Search Engine
 const typeDefs = `
     type Query {
         me: User
     }
 
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        removeBook(bookId: String!): User
+    }
 
-`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: String!
+        authors: [String]
+        description: String
+        title: String!
+        image: String
+        link: String
+    }
+
+    input BookInput {
+        bookId: String!
+        authors: [String]
+        description: String
+        title: String!
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+`;
+
+// Export the typeDefs to be used in other files
+module.exports = typeDefs;
