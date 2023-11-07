5e5ffb486acffb72ffc851573d3cde1b5c455305
kevindimayuga <k.dimayuga22@gmail.com>
2023-11-06 22:30:02 -0600

    updating app.jsx to create apollo provider

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -6,12 +6,22 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import Navbar from './components/Navbar';
 
+
+const client = new ApolloClient({
+  // This is the URL of the GraphQL server.
+  uri: '/graphql',
+  // This is where Apollo Client will cache our data so that we don't have to query the server each time we want to see the data.
+  cache: new InMemoryCache(),
+});
+
+// wrapped Navbar and Outlet in ApolloProvider component and passed the client variable as the value for the client prop.
+// ApolloProvider is a special type of React component that we'll use to provide and data to all of the other components.
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
